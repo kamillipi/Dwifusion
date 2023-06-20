46d9e35bede6d4b9cb5818f581f150a74a97926d
kamillipi <lipinski.kamil@yahoo.pl>
2023-06-20 12:22:23 +0200

    add scaling

--- atlsegment.m
+++ atlsegment.m
@@ -1,9 +1,13 @@
-function signal = atlsegment(data,nbvals)
+function signal = atlsegment(data)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 atlas=niftiread('ROI_MNI_V7.nii');
-signal=zeros(170,nbvals);
-for j = 1:nbvals
+data_size=size(data,1:4);
+if data_size(1:3)~=size(atlas)
+    atlas=imresize3(atlas,data_size(1:3),method="nearest");
+end
+signal=zeros(170,data_size(4));
+for j = 1:data_size(4)
     vol=squeeze(data(:,:,:,j));
     for i = 1:170
         signal(i,j)=sum(vol(atlas==i),"all");
